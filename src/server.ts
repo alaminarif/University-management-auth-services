9dec35d3685a67af36e5ce9c0d2e00c30a3707f3
al-amin <arifurr231@gmail.com>
2023-05-30 10:01:45 +0600

    done cors mongose express setup

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import config from "./config";
+import app from "./app";
+
+const bootstrap = async () => {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log(`🛢   Database is connected successfully`);
+
+    app.listen(config.port, () => {
+      console.log(`Application  listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log("failed to database: ", error);
+  }
+};
+
+bootstrap();
